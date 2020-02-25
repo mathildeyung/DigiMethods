4684f63d355cbade193ac099f27512e265db9507
mathildeyung <60779723+mathildeyung@users.noreply.github.com>
2020-02-25 10:59:44 +0100

    Test
    test

---
+++ AppCred.py
@@ -0,0 +1,14 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2511
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;}
+{\*\expandedcolortbl;;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww10800\viewh8400\viewkind0
+\pard\tx566\tx1133\tx1700\tx2267\tx2834\tx3401\tx3968\tx4535\tx5102\tx5669\tx6236\tx6803\pardirnatural\partightenfactor0
+
+\f0\fs24 \cf0 #My application credentials for twitter APIs\
+\
+consumer_key = "FULYEYV7jl8leJSPd1s1w3cDT"\
+consumer_secret = "d2Cch1IQ6j14rexgL95vpSbVZdMNmpS4mJEyrsb5HvfiDwi5Up"\
+\
+access_token = "1037999668343517184-5TV8TkeV13lMZHzUju1NXP0ArcMw7w"\
+access_token_secret = "BXXbmNSsr2UyPTE4At0f0zN1kzeCsxzLeDILDLtneQ5lA"}
